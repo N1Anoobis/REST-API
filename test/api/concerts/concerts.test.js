641fdca1047e45310e2792c5322062b689834309
N1Anoobis <mrbond84@hotmail.com>
2020-10-03 17:59:51 +0200

    Add test for GET request

--- test/api/concerts/concerts.test.js
+++ test/api/concerts/concerts.test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../../server.js');
+const server = require('../../../server');
 chai.use(chaiHttp);
 const Concert = require('../../../models/concerts.model');
 const expect = chai.expect;
@@ -8,30 +8,44 @@ const request = chai.request;
 
 describe('GET /api/concerts', () => {
     before(async () => {
-        const testConcertOne = new Concert({ _id: '5d9f1140f10a81216cfd4408', performer: 'John Rambo', genre: 'Pop' , price: 25, day: 1});
+        const testConcertOne = new Concert({ performer: 'John Rambo', genre: 'Pop' , price: '25', day: 1, image: 'abc.jpg'});
         await testConcertOne.save();
       
-        const testConcertTwo = new Concert({ _id: '5d9f1159f81ce8d1ef2bee48', performer: 'Elvis Alien',genre: 'Metal', price: 30, day: 2, });
+        const testConcertTwo = new Concert({ performer: 'Elvis Alien',genre: 'Metal', price: '30', day: 2, image: 'abc.jpg' });
         await testConcertTwo.save();
       });
 
-    it('/ should return concerts including given performer', () => {
+    it('/ should return concerts including given performer', async() => {
         const res = await request(server).get('/api/concerts/performer/John Rambo');
         expect(res.status).to.be.equal(200);
         expect(res.body).to.be.an('Array');
         expect(res.body).to.not.be.null;
     });
   
-    it('/ should return concerts including given genre', () => {
-  
+    it('/ should return concerts including given genre', async() => {
+      const res = await request(server).get('/api/concerts/genre/Pop');
+      expect(res.status).to.be.equal(200);
+      expect(res.body).to.be.an('Array');
+      expect(res.body).to.not.be.null;
     });
   
-    it('/ should return concerts including given range of prices', () => {
-  
+    it('/ should return concerts including given range of prices', async() => {
+     try {
+      const res = await request(server).get('/api/concerts/price/1/100');
+      expect(res.status).to.be.equal(200);
+      expect(res.body).to.be.an('Array');
+      expect(res.body.length).to.be.equal(2);
+      expect(res.body).to.not.be.null;
+    }catch (error){
+      console.log(error)
+    }
     });
   
-    it('/ should return concerts including given day', () => {
-  
+    it('/ should return concerts including given day', async() => {
+      const res = await request(server).get('/api/concerts/day/1');
+      expect(res.status).to.be.equal(200);
+      expect(res.body).to.be.an('Array');
+      expect(res.body).to.not.be.null;
     });
     after(async () => {
         await Concert.deleteMany();
