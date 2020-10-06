5f986ceeb1a797273d70e1e62ef6bde3221019b6
N1Anoobis <mrbond84@hotmail.com>
2020-10-06 16:15:13 +0200

    Add helmet and snyc

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const socket = require('socket.io');
 const mongoose = require('mongoose')
 const app = express();
+const helmet = require('helmet');
 
 const testimonialsRoutes = require('./routes/testimonials.routes.js');
 const concertsRoutes = require('./routes/concerts.routes.js');
@@ -13,6 +14,7 @@ app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
+app.use(helmet());
 app.use(cors());
 
 app.use((req, res, next) => {
